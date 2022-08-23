3d9d533847cccf6c09803e2605ef065f80ddd564
SackMathen <jmw1471@msstate.edu>
2022-08-23 16:12:38 -0500

    small file.

---
+++ client_app.py
@@ -0,0 +1,8 @@
+def connect():
+    global username, client
+    if len(entName.get()) < 1:
+        tk.messagebox.showerror(title="ERROR!!!", message="You MUST enter your first name <e.g. John>")
+    else:
+        username = entName.get()
+        connect_to_server(username)
+
