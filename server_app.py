a9b36d54757d5902e1c9ad9b080ec3479b37d938
BuildTools <unconfigured@null.spigotmc.org>
2022-08-30 20:54:40 -0500

    fixed server code.

--- server_app.py
+++ server_app.py
@@ -1,67 +1,38 @@
-# file is complete for starting and stopping the server. need to add buttons for dark mode and voice.
-# could also do something for commands. well look into it later.
 import tkinter as tk
 import socket
 import threading
 
-
-# start of the server GUI
 
 window = tk.Tk()
 window.title("Sever")
 
 # Top frame consisting of two buttons widgets (i.e. btnStart, btnStop)
 topFrame = tk.Frame(window)
-btnStart = tk.Button(topFrame, text="Connect", command=lambda: start_server())
+btnStart = tk.Button(topFrame, text="Connect", command=lambda : start_server())
 btnStart.pack(side=tk.LEFT)
-btnStop = tk.Button(topFrame, text="Stop", command=lambda: stop_server(), state=tk.DISABLED)
+btnStop = tk.Button(topFrame, text="Stop", command=lambda : stop_server(), state=tk.DISABLED)
 btnStop.pack(side=tk.LEFT)
 topFrame.pack(side=tk.TOP, pady=(5, 0))
 
 # Middle frame consisting of two labels for displaying the host and port info
 middleFrame = tk.Frame(window)
-lblHost = tk.Label(middleFrame, text="Host: X.X.X.X")
+lblHost = tk.Label(middleFrame, text = "Host: X.X.X.X")
 lblHost.pack(side=tk.LEFT)
-lblPort = tk.Label(middleFrame, text="Port:XXXX")
+lblPort = tk.Label(middleFrame, text = "Port:XXXX")
 lblPort.pack(side=tk.LEFT)
 middleFrame.pack(side=tk.TOP, pady=(5, 0))
 
 # The client frame shows the client area
 clientFrame = tk.Frame(window)
-lblLine = tk.Label(clientFrame, text="*******************CLIENT LIST*****************").pack()
+lblLine = tk.Label(clientFrame, text="**********Client List**********").pack()
 scrollBar = tk.Scrollbar(clientFrame)
 scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
-tkDisplay = tk.Text(clientFrame, height=15, width=40)
+tkDisplay = tk.Text(clientFrame, height=15, width=30)
 tkDisplay.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
-# Insert demo connected user names
-tkDisplay.insert(tk.END, "User 1\n")
-tkDisplay.insert(tk.END, "User 2\n")
-tkDisplay.insert(tk.END, "User 3\n")
-tkDisplay.insert(tk.END, "User 4\n")
-tkDisplay.insert(tk.END, "User 5\n")
-tkDisplay.insert(tk.END, "User 6\n")
-tkDisplay.insert(tk.END, "User 7\n")
-tkDisplay.insert(tk.END, "User 8\n")
 scrollBar.config(command=tkDisplay.yview)
 tkDisplay.config(yscrollcommand=scrollBar.set, background="#F4F6F7", highlightbackground="grey", state="disabled")
 clientFrame.pack(side=tk.BOTTOM, pady=(5, 10))
 
-
-def start_server():
-    btnStart.config(state=tk.DISABLED)
-    btnStop.config(state=tk.NORMAL)
-
-
-def stop_server():
-    btnStart.config(state=tk.NORMAL)
-    btnStop.config(state=tk.DISABLED)
-
-# end of server GUI
-
-window.mainloop()
-
-
-# beginning of the server backend.
 
 server = None
 HOST_ADDR = "0.0.0.0"
@@ -78,6 +49,9 @@ def start_server():
     btnStop.config(state=tk.NORMAL)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(socket.AF_INET)
+    print(socket.SOCK_STREAM)
+
     server.bind((HOST_ADDR, HOST_PORT))
     server.listen(5)  # server is listening for client connection
 
@@ -86,7 +60,6 @@ def start_server():
     lblHost["text"] = "Host: " + HOST_ADDR
     lblPort["text"] = "Port: " + str(HOST_PORT)
 
-# end of start server function
 
 # Stop server function
 def stop_server():
