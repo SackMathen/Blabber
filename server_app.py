c19bea621256a51ddc44326fcd9a3f630f03598e
BuildTools <unconfigured@null.spigotmc.org>
2022-08-30 20:10:50 -0500

    finished server gui and backend. looking to add user functionality at a later date

--- server_app.py
+++ server_app.py
@@ -85,3 +85,85 @@ def start_server():
     lblPort["text"] = "Port: " + str(HOST_PORT)
 
 # end of start server function
+
+# Stop server function
+def stop_server():
+    global server
+    btnStart.config(state=tk.NORMAL)
+    btnStop.config(state=tk.DISABLED)
+
+
+def accept_clients(the_server, y):
+    while True:
+        client, addr = the_server.accept()
+        clients.append(client)
+
+        # use a thread so as not to clog the gui thread
+        threading._start_new_thread(send_receive_client_message, (client, addr))
+
+
+# Function to receive message from current client AND
+# Send that message to other clients
+def send_receive_client_message(client_connection, client_ip_addr):
+    global server, client_name, clients, clients_addr
+    client_msg = " "
+
+    # send welcome message to client
+    client_name  = client_connection.recv(4096).decode()
+    welcome_msg = "Welcome " + client_name + ". Use 'exit' to quit"
+    client_connection.send(welcome_msg.encode())
+
+    clients_names.append(client_name)
+
+    update_client_names_display(clients_names)  # update client names display
+
+
+    while True:
+        data = client_connection.recv(4096).decode()
+        if not data: break
+        if data == "exit": break
+
+        client_msg = data
+
+        idx = get_client_index(clients, client_connection)
+        sending_client_name = clients_names[idx]
+
+        for c in clients:
+            if c != client_connection:
+                server_msg = str(sending_client_name + "->" + client_msg)
+                c.send(server_msg.encode())
+
+    # find the client index then remove from both lists(client name list and connection list)
+    idx = get_client_index(clients, client_connection)
+    del clients_names[idx]
+    del clients[idx]
+    server_msg = "BYE!"
+    client_connection.send(server_msg.encode())
+    client_connection.close()
+
+    update_client_names_display(clients_names)  # update client names display
+
+
+# Return the index of the current client in the list of clients
+def get_client_index(client_list, curr_client):
+    idx = 0
+    for conn in client_list:
+        if conn == curr_client:
+            break
+        idx = idx + 1
+
+    return idx
+
+
+# Update client name display when a new client connects OR
+# When a connected client disconnects
+def update_client_names_display(name_list):
+    tkDisplay.config(state=tk.NORMAL)
+    tkDisplay.delete('1.0', tk.END)
+
+    for c in name_list:
+        tkDisplay.insert(tk.END, c+"\n")
+    tkDisplay.config(state=tk.DISABLED)
+
+
+window.mainloop()
