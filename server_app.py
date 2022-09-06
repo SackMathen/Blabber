2e086060d02c0c2a52b78a7ca1324e4077a12a67
John Mathen Wierengo <75157855+SackMathen@users.noreply.github.com>
2022-09-06 07:55:39 -0500

    server functionality change
    server was local network only. now it asks for host address and port

--- server_app.py
+++ server_app.py
@@ -1,144 +1,63 @@
-import tkinter as tk
-import socket
-import threading
+#!/usr/bin/env python3
+"""Server for multithreading (asynchronous) chat application."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 
 
-window = tk.Tk()
-window.title("Sever")
-
-# Top frame consisting of two buttons widgets (i.e. btnStart, btnStop)
-topFrame = tk.Frame(window)
-btnStart = tk.Button(topFrame, text="Connect", command=lambda : start_server())
-btnStart.pack(side=tk.LEFT)
-btnStop = tk.Button(topFrame, text="Stop", command=lambda : stop_server(), state=tk.DISABLED)
-btnStop.pack(side=tk.LEFT)
-topFrame.pack(side=tk.TOP, pady=(5, 0))
-
-# Middle frame consisting of two labels for displaying the host and port info
-middleFrame = tk.Frame(window)
-lblHost = tk.Label(middleFrame, text = "Host: X.X.X.X")
-lblHost.pack(side=tk.LEFT)
-lblPort = tk.Label(middleFrame, text = "Port:XXXX")
-lblPort.pack(side=tk.LEFT)
-middleFrame.pack(side=tk.TOP, pady=(5, 0))
-
-# The client frame shows the client area
-clientFrame = tk.Frame(window)
-lblLine = tk.Label(clientFrame, text="**********Client List**********").pack()
-scrollBar = tk.Scrollbar(clientFrame)
-scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
-tkDisplay = tk.Text(clientFrame, height=15, width=30)
-tkDisplay.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
-scrollBar.config(command=tkDisplay.yview)
-tkDisplay.config(yscrollcommand=scrollBar.set, background="#F4F6F7", highlightbackground="grey", state="disabled")
-clientFrame.pack(side=tk.BOTTOM, pady=(5, 10))
-
-
-server = None
-HOST_ADDR = "0.0.0.0"
-HOST_PORT = 8080
-client_name = " "
-clients = []
-clients_names = []
-
-
-# Start server function
-def start_server():
-    global server, HOST_ADDR, HOST_PORT # code is fine without this
-    btnStart.config(state=tk.DISABLED)
-    btnStop.config(state=tk.NORMAL)
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(socket.AF_INET)
-    print(socket.SOCK_STREAM)
-
-    server.bind((HOST_ADDR, HOST_PORT))
-    server.listen(5)  # server is listening for client connection
-
-    threading._start_new_thread(accept_clients, (server, " "))
-
-    lblHost["text"] = "Host: " + HOST_ADDR
-    lblPort["text"] = "Port: " + str(HOST_PORT)
-
-
-# Stop server function
-def stop_server():
-    global server
-    btnStart.config(state=tk.NORMAL)
-    btnStop.config(state=tk.DISABLED)
-
-
-def accept_clients(the_server, y):
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
     while True:
-        client, addr = the_server.accept()
-        clients.append(client)
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
 
-        # use a thread so as not to clog the gui thread
-        threading._start_new_thread(send_receive_client_message, (client, addr))
 
+def handle_client(client):  # Takes client socket as argument.
+    """Handles a single client connection."""
 
-# Function to receive message from current client AND
-# Send that message to other clients
-def send_receive_client_message(client_connection, client_ip_addr):
-    global server, client_name, clients, clients_addr
-    client_msg = " "
-
-    # send welcome message to client
-    client_name  = client_connection.recv(4096).decode()
-    welcome_msg = "Welcome " + client_name + ". Use 'exit' to quit"
-    client_connection.send(welcome_msg.encode())
-
-    clients_names.append(client_name)
-
-    update_client_names_display(clients_names)  # update client names display
-
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
 
     while True:
-        data = client_connection.recv(4096).decode()
-        if not data: break
-        if data == "exit": break
-
-        client_msg = data
-
-        idx = get_client_index(clients, client_connection)
-        sending_client_name = clients_names[idx]
-
-        for c in clients:
-            if c != client_connection:
-                server_msg = str(sending_client_name + "->" + client_msg)
-                c.send(server_msg.encode())
-
-    # find the client index then remove from both lists(client name list and connection list)
-    idx = get_client_index(clients, client_connection)
-    del clients_names[idx]
-    del clients[idx]
-    server_msg = "BYE!"
-    client_connection.send(server_msg.encode())
-    client_connection.close()
-
-    update_client_names_display(clients_names)  # update client names display
-
-
-# Return the index of the current client in the list of clients
-def get_client_index(client_list, curr_client):
-    idx = 0
-    for conn in client_list:
-        if conn == curr_client:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name + ": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
             break
-        idx = idx + 1
-
-    return idx
 
 
-# Update client name display when a new client connects OR
-# When a connected client disconnects
-def update_client_names_display(name_list):
-    tkDisplay.config(state=tk.NORMAL)
-    tkDisplay.delete('1.0', tk.END)
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
 
-    for c in name_list:
-        tkDisplay.insert(tk.END, c+"\n")
-    tkDisplay.config(state=tk.DISABLED)
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
 
 
-window.mainloop()
+clients = {}
+addresses = {}
+
+HOST = ''
+PORT = 33000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
