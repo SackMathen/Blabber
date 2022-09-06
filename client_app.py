02032a43fa0c4f6a29457d376a74868873bfe754
John Mathen Wierengo <75157855+SackMathen@users.noreply.github.com>
2022-09-06 07:56:50 -0500

    updated client app
    updated client app that asks for a address and port to connect to

--- client_app.py
+++ client_app.py
@@ -1,128 +1,73 @@
-import tkinter as tk
-from tkinter import messagebox
-import socket
-import threading
-
-window = tk.Tk()
-window.title("Client")
-username = " "
+#!/usr/bin/env python3
+"""Script for Tkinter GUI chat client."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
 
 
-topFrame = tk.Frame(window)
-lblName = tk.Label(topFrame, text = "Name:").pack(side=tk.LEFT)
-entName = tk.Entry(topFrame)
-entName.pack(side=tk.LEFT)
-btnConnect = tk.Button(topFrame, text="Connect", command=lambda : connect())
-btnConnect.pack(side=tk.LEFT)
-#btnConnect.bind('<Button-1>', connect)
-topFrame.pack(side=tk.TOP)
-
-displayFrame = tk.Frame(window)
-lblLine = tk.Label(displayFrame, text="*********************************************************************").pack()
-scrollBar = tk.Scrollbar(displayFrame)
-scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
-tkDisplay = tk.Text(displayFrame, height=20, width=55)
-tkDisplay.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
-tkDisplay.tag_config("tag_your_message", foreground="blue")
-scrollBar.config(command=tkDisplay.yview)
-tkDisplay.config(yscrollcommand=scrollBar.set, background="#F4F6F7", highlightbackground="grey", state="disabled")
-displayFrame.pack(side=tk.TOP)
-
-
-bottomFrame = tk.Frame(window)
-tkMessage = tk.Text(bottomFrame, height=2, width=55)
-tkMessage.pack(side=tk.LEFT, padx=(5, 13), pady=(5, 10))
-tkMessage.config(highlightbackground="grey", state="disabled")
-tkMessage.bind("<Return>", (lambda event: getChatMessage(tkMessage.get("1.0", tk.END))))
-bottomFrame.pack(side=tk.BOTTOM)
-
-
-def connect():
-    global username, client
-    if len(entName.get()) < 1:
-        tk.messagebox.showerror(title="ERROR!!!", message="You MUST enter your first name <e.g. John>")
-    else:
-        username = entName.get()
-        connect_to_server(username)
-
-
-# network client
-client = None
-HOST_ADDR = "0.0.0.0"
-HOST_PORT = 8080
-
-def connect_to_server(name):
-    global client, HOST_PORT, HOST_ADDR
-    try:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((HOST_ADDR, HOST_PORT))
-        client.send(name.encode()) # Send name to server after connecting
-
-        entName.config(state=tk.DISABLED)
-        btnConnect.config(state=tk.DISABLED)
-        tkMessage.config(state=tk.NORMAL)
-
-        # start a thread to keep receiving message from server
-        # do not block the main thread :)
-        threading._start_new_thread(receive_message_from_server, (client, "m"))
-    except Exception as e:
-        tk.messagebox.showerror(title="ERROR!!!", message="Cannot connect to host: " + HOST_ADDR + " on port: " + str(HOST_PORT) + " Server may be Unavailable. Try again later")
-
-
-def receive_message_from_server(sck, m):
+def receive():
+    """Handles receiving of messages."""
     while True:
-        from_server = sck.recv(4096).decode()
-
-        if not from_server: break
-
-        # display message from server on the chat window
-
-        # enable the display area and insert the text and then disable.
-        # why? Apparently, tkinter does not allow us insert into a disabled Text widget :(
-        texts = tkDisplay.get("1.0", tk.END).strip()
-        tkDisplay.config(state=tk.NORMAL)
-        if len(texts) < 1:
-            tkDisplay.insert(tk.END, from_server)
-        else:
-            tkDisplay.insert(tk.END, "\n\n"+ from_server)
-
-        tkDisplay.config(state=tk.DISABLED)
-        tkDisplay.see(tk.END)
-
-        # print("Server says: " +from_server)
-
-    sck.close()
-    window.destroy()
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg_list.insert(tkinter.END, msg)
+        except OSError:  # Possibly client has left the chat.
+            break
 
 
-def getChatMessage(msg):
-
-    msg = msg.replace('\n', '')
-    texts = tkDisplay.get("1.0", tk.END).strip()
-
-    # enable the display area and insert the text and then disable.
-    # why? Apparently, tkinter does not allow use insert into a disabled Text widget :(
-    tkDisplay.config(state=tk.NORMAL)
-    if len(texts) < 1:
-        tkDisplay.insert(tk.END, "You->" + msg, "tag_your_message") # no line
-    else:
-        tkDisplay.insert(tk.END, "\n\n" + "You->" + msg, "tag_your_message")
-
-    tkDisplay.config(state=tk.DISABLED)
-
-    send_mssage_to_server(msg)
-
-    tkDisplay.see(tk.END)
-    tkMessage.delete('1.0', tk.END)
+def send(event=None):  # event is passed by binders.
+    """Handles sending of messages."""
+    msg = my_msg.get()
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        top.quit()
 
 
-def send_mssage_to_server(msg):
-    client_msg = str(msg)
-    client.send(client_msg.encode())
-    if msg == "exit":
-        client.close()
-        window.destroy()
-    print("Sending message")
+def on_closing(event=None):
+    """This function is to be called when the window is closed."""
+    my_msg.set("{quit}")
+    send()
 
+top = tkinter.Tk()
+top.title("Chatter")
 
-window.mainloop()
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()  # For the messages to be sent.
+my_msg.set("Type your messages here.")
+scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+# ----Now comes the sockets part----
+# Also need to make a gui portion to transition from entering host address
+# and port to displaying the chat once connected.
+HOST = input('Enter host: ')
+PORT = input('Enter port: ')
+if not PORT:
+    PORT = 33000
+else:
+    PORT = int(PORT)
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()  # Starts GUI execution.
